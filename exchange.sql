0999b20818115b0c01a516d23dd57aeed65e2038
Kedar Iyer <vixidev99@protonmail.com>
2025-08-12 19:36:10 -0400

    orders table

--- exchange.sql
+++ exchange.sql
@@ -5,16 +5,37 @@ CREATE TABLE currencies(
 );
 
 CREATE TABLE pairs(
+	id INT NOT NULL AUTO_INCREMENT,
 	base_currency varchar(255) not null,
 	quote_currency varchar(255) not null,
-	PRIMARY KEY(base_currency, quote_currency),
+	PRIMARY KEY(id),
 	FOREIGN KEY(base_currency) REFERENCES currencies(name),
-	FOREIGN KEY(quote_currency) REFERENCES currencies(name)
+	FOREIGN KEY(quote_currency) REFERENCES currencies(name),
+	UNIQUE(base_currency, quote_currency)
 );
 
 CREATE TABLE balances(
 	user CHAR(32),
 	currency varchar(255),
 	balance INT NOT NULL DEFAULT(0),
-	PRIMARY KEY(user, currency)
+	update_timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY(user, currency),
+	FOREIGN KEY(user) REFERENCES mysql.user(User),
+	FOREIGN KEY(currency) REFERENCES currencies(name)
+);
+
+CREATE TABLE orders(
+	id INT NOT NULL AUTO_INCREMENT,
+	user CHAR(32) NOT NULL,
+	pair_id INT NOT NULL,
+	price DOUBLE NOT NULL,
+	side ENUM('buy','sell') NOT NULL,
+	insert_timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	update_timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY(pair_id) REFERENCES pairs(id),
+	FOREIGN KEY(user) REFERENCES mysql.user(User),
+);
+
+CREATE TABLE fills(
 );
